6b40aebdad83319475af0fc5bf37a9847481b1a8
ajvsubotich <145725821+ajvsubotich@users.noreply.github.com>
2024-01-21 22:57:19 +0000

    se quita el report de mocka

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,7 @@ const { defineConfig } = require('cypress')
 
 
 
-module.exports = defineConfig({
-  reporter: 'mochawesome',  
+module.exports = defineConfig({  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
